e03bbacaa5182be4cb0c5e6830d93c78b3164c23
Jhonatan Morais <jhonatan.morais@gmail.com>
2020-12-03 09:00:54 -0300

    new: mail test provided

---
+++ tests/Feature/MailTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MailerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function anyone_can_send_an_email()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/api/mails', [
+            'title' => 'My email title',
+            'recipients' => 'email1@newmail.com',
+            'content_type' => 'richText',
+            'body'  => 'My email message'
+        ])->assertStatus(201);
+
+        $this->assertCount(1, Mail::all());
+        $mail = Mail::find(1);
+        $this->assertEquals('My email title', $mail->title);
+        $this->assertEquals('email1@newmail.com', $mail->recipients);
+        $this->assertEquals('richText', $mail->content_type);
+        $this->assertEquals('body', $mail->body);
+        $response->assertJson([
+            'data' => [
+                'type' => 'mails',
+                'id' => $mail->id,
+                'attributes' => [
+                    'title' => $mail->title,
+                    'recipients' => $mail->recipients,
+                    'content_type' => $mail->content_type,
+                    'body' => $mail->body,
+                ],
+
+            ],
+            'links' => [
+                'self' => url('/api/mails/' . $mail->id)
+            ]
+        ]);
+    }
+}
